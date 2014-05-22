906f8c5ef8f7b7f91fd53377852a5c7d41f9fd27
stan Mazurek <stan@mazurek.me>
2014-05-22 13:09:22 +0100

    use psych

--- lib/forever.rb
+++ lib/forever.rb
@@ -1,10 +1,8 @@
-require 'yaml' unless defined?(YAML)
+require 'yaml'
 require "forever/extensions"
 require "forever/every"
 require "forever/base"
 require "forever/version"
-
-YAML::ENGINE.yamler = "syck" if defined?(YAML::ENGINE)
 
 FOREVER_PATH = ENV['FOREVER_PATH'] ||= File.expand_path("~/.foreverb") unless defined?(FOREVER_PATH)
 path = File.dirname(FOREVER_PATH)
